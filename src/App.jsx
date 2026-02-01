755caf14b00311249dc09fb8ce443e824b7aaf94
iandang312 <iandang312@gmail.com>
2026-02-01 17:41:42 -0600

    changed gif and question

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function Page() {
           />
           <img
             className="h-[230px] rounded-lg shadow-lg"
-            src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://media.gifdb.com/eating-head-animated-bear-9gxeelezvfi6elg9.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
             Lily, may I be your Valentine? -Ian 🥹
